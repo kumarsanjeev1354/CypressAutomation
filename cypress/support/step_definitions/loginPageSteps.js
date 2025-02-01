a9ca27034be76f4b9686eeefdcc9d6c95d9abaa0
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2025-02-01 18:15:22 +0530

    Error Check coommit
    Signed-off-by: kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>

--- cypress/support/step_definitions/loginPageSteps.js
+++ cypress/support/step_definitions/loginPageSteps.js
@@ -5,3 +5,5 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 Given('I want to login to the appliaction', () => {
   cy.visit('https://opensource-demo.orangehrmlive.com')
 })
+
+Given('i want')
