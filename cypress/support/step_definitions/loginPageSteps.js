d0585657e8f451f0d64a3655e8c024d532b6b742
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2025-02-16 23:53:56 +0530

    commit error removed
    Signed-off-by: kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>

--- cypress/support/step_definitions/loginPageSteps.js
+++ cypress/support/step_definitions/loginPageSteps.js
@@ -4,7 +4,6 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 Given('I want to login to the appliaction', () => {
   cy.visit('https://www.google.com')
-  cy.visit('https://www.google.com')//This will be an error
-})
+  
 })
 
