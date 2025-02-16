e96cc9f05668728d9f282c7f006f75df78efea13
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2025-02-16 15:16:08 +0530

    Added new changes in ligon page
    Signed-off-by: kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>

--- cypress/support/step_definitions/loginPageSteps.js
+++ cypress/support/step_definitions/loginPageSteps.js
@@ -4,5 +4,7 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 Given('I want to login to the appliaction', () => {
   cy.visit('https://www.google.com')
+  cy.visit('https://www.google.com')//This will be an error
+})
 })
 
